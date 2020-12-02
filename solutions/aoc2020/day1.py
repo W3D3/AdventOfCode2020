39a7613c87b050c1dde00a0ebfbfa82d495c04ff
Christoph Wedenig <christoph@wedenig.org>
2020-12-02 19:42:29 +0100

    Optimized day 1 solution

--- solutions/aoc2020/day1.py
+++ solutions/aoc2020/day1.py
@@ -1,5 +1,6 @@
 from aocd import data
 from aocd import submit
+import timeit
 
 def part_a(data):
     nums = list(map(int, data.strip().split("\n")))
@@ -22,6 +23,23 @@ def part_b(data):
                             return str(nums[i] * nums[j] * nums[k])
     return None
 
+def part_a_set(data):
+    nums = set(map(int, data.strip().split("\n")))
+    for num in nums:
+        diff = 2020 - num
+        if diff in nums:
+            return str(num * diff)
+    return None
+
+def part_b_set(data):
+    nums = set(map(int, data.strip().split("\n")))
+    for num in nums:
+        for num2 in nums:
+            diff = 2020 - num - num2
+            if diff in nums:
+                return str(num * num2 * diff)
+    return None
+
 
 test_data = """\
 1721
@@ -32,14 +50,23 @@ test_data = """\
 1456
 """
 
+def timing_differences():
+    loops = 10000
+    print('part_a_set', timeit.timeit('part_a_set(data)', number=loops, globals=globals()))
+    print('part_a', timeit.timeit('part_a(data)', number=loops, globals=globals()))
+
+    print('part_b_set', timeit.timeit('part_b_set(data)', number=loops, globals=globals()))
+    print('part_b', timeit.timeit('part_b(data)', number=loops, globals=globals()))
 
 if __name__ == "__main__":
-    assert part_a(test_data) == "514579"
-    assert part_b(test_data) == "241861950"
-    solution_a = part_a(data)
+    # timing_differences()
+
+    assert part_a_set(test_data) == "514579"
+    assert part_b_set(test_data) == "241861950"
+    solution_a = part_a_set(data)
     print(solution_a)
-    submit(solution_a, part="a", day=1, year=2020)
-    solution_b = part_b(data)
+    # submit(solution_a, part="a", day=1, year=2020)
+    solution_b = part_b_set(data)
     print(solution_b)
-    submit(solution_b, part="b", day=1, year=2020)
+    # submit(solution_b, part="b", day=1, year=2020)
 
