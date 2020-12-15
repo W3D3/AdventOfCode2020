a92e762179a9ecd390074f00d34d1177aa8304e3
Christoph Wedenig <christoph@wedenig.org>
2020-12-15 19:49:50 +0100

    Solved and refactored day 15

--- solutions/aoc2020/day15.py
+++ solutions/aoc2020/day15.py
@@ -1,29 +1,12 @@
-import itertools
-
 from aocd import data
 from aocd import submit
-from parse import parse
-
 
 def part_a(data):
-    starting_nums = parse_data(data)
-    history = []
+    return get_number_spoken_at(parse_data(data), 2020)
 
-    for num in starting_nums:
-        history.append(num)
 
-    for i in range(len(starting_nums)+1, 2021):
-        print(history)
-        last_spoken_index = find_last(history[:-1], history[-1])
-        if last_spoken_index >= 0:
-            diff = (i - 1) - (last_spoken_index + 1) # -1 as we are analyzing the last turn, +1 as we are 1 indexed in turns
-            # print(last_spoken_index+1, i - 1, "=", diff)
-            history.append(diff)
-            print(i, "speaks", diff)
-        else:
-            history.append(0)
-
-    return history[-1]
+def part_b(data):
+    return get_number_spoken_at(parse_data(data), 30_000_000)
 
 
 def find_last(list, element):
@@ -32,8 +15,40 @@ def find_last(list, element):
             return len(list) - 1 - r_idx
     return -1
 
-def part_b(data):
-    return None
+
+def get_number_spoken_at(starting_nums, index):
+    dict = {}
+    last_spoken = None
+
+    for idx, num in enumerate(starting_nums):
+        dict[num] = (idx + 1, None)  # Hold the two last indices who said this number
+
+    last_spoken = starting_nums[-1]
+    for i in range(len(starting_nums) + 1, index + 1):
+        first_idx, second_idx = dict[last_spoken]  # get last spoken idx
+        if second_idx is None:
+            # this was the first occurence!
+            spoken = 0
+            update_tuple(dict, spoken, i)
+        else:
+            diff = second_idx - first_idx
+            spoken = diff
+            update_tuple(dict, spoken, i)
+        last_spoken = spoken
+
+    return last_spoken
+
+
+def update_tuple(dict, spoken, new_idx):
+    if spoken not in dict:
+        dict[spoken] = (new_idx, None)
+
+    first, second = dict[spoken]
+    if second is None:
+        dict[spoken] = (first, new_idx)
+    else:
+        dict[spoken] = (second, new_idx)
+
 
 def parse_data(data):
     return list(map(int, data.strip().split(",")))
@@ -44,11 +59,10 @@ test_data = """
 """
 
 if __name__ == "__main__":
-    part_a(test_data)
     assert part_a(test_data) == 436
-    solution_a = part_a(data)
+    # solution_a = part_a(data)
     # submit(solution_a, part="a", day=15, year=2020)
 
-    # assert part_b(test_data) == 0
+    assert part_b(test_data) == 175594
     # solution_b = part_b(data)
     # submit(solution_b, part="b", day=15, year=2020)
